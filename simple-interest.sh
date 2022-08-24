5a74ce62263c2ea59ea0891566a71535d3afa8a0
hendrikjoosten <18636025+hendrikjoosten@users.noreply.github.com>
2022-08-24 14:25:46 +0200

    added username

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,7 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # hendrikjoosten
+   # Additional Authors: hendrikjoosten
 
    # Input:
    # p, principal amount
